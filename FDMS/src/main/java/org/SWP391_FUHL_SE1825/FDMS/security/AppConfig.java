da18e878e4a3e129b448719c532880104cf883cf
huytran2810 <140027999+huytran2810@users.noreply.github.com>
2024-08-03 05:13:26 +0700

    Update AppConfig.java

--- FDMS/src/main/java/org/SWP391_FUHL_SE1825/FDMS/security/AppConfig.java
+++ FDMS/src/main/java/org/SWP391_FUHL_SE1825/FDMS/security/AppConfig.java
@@ -11,9 +11,9 @@ public class AppConfig {
     @Bean
     public Cloudinary cloudinaryConfig() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dk9j6rq01",
-                "api_key", "481378768279841",
-                "api_secret", "cdp41PCTXyl4nnpxFps200wolLk"));
+                "cloud_name", "",
+                "api_key", "",
+                "api_secret", ""));
     }
 
 }
